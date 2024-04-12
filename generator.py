6197ad337e25a2f05689aafa0e9acdd8541bf495
fnfuente <fnfuente@inf.utfsm.cl>
2024-04-12 11:36:08 -0400

    feat: pass generator and main logic

--- generator.py
+++ generator.py
@@ -1,10 +1,15 @@
+import random
+
 class GeneratorService:
     DEFAULT_CHARS = "abcdefghijklmnopqrstuvwxyz0123456789"
 
-    def __init__(self, salt, max_length=16, chars=DEFAULT_CHARS) -> None:
+    def __init__(self, salt, max_length=25, chars=DEFAULT_CHARS) -> None:
         self.salt = salt
         self.max_length = max_length
         self.chars = chars
 
-    def generate_password(self) -> str:
-        pass
+    def generate_password(self, length):
+        length = min(length, self.max_length)
+        password = ''.join(random.choice(self.chars) for _ in range(length))
+
+        return password
--- main.py
+++ main.py
@@ -1,7 +1,57 @@
+import os
+import time
 from database import Database
+from generator import GeneratorService
 
 if __name__ == '__main__':
-    db = Database()
+    #db = Database()
+
+    print("Welcome to MyPass!\n")
+
+    flag_pass = False # This flag will be used to check if the main password has already been entered
 
     while True:
-        break
+
+        if not flag_pass:
+            main_pass = input("Main password: ")
+            flag_pass = True
+
+        if True: # TODO: Check if main_pass is correct in DB
+            os.system('cls' if os.name=='nt' else 'clear')
+
+            print("----- MYPASS MENU -----\n")
+            print("1. Add Password")
+            print("2. Get Password")
+            print("3. Generate Password")
+            print("4. Exit\n")
+
+            choice = input("Enter your choice: ")
+
+            if choice == '1':
+                platform = input("Enter platform: ")
+                password = input("Enter password: ")
+                # TODO: Implement add password
+
+            elif choice == '2':
+                platform = input("Enter platform: ")
+                # TODO: Implement get password
+
+            elif choice == '3':
+                platform = input("Enter platform: ")
+                length = int(input("Enter length: "))
+
+                generator = GeneratorService("salt", length)
+                generated_password = generator.generate_password(length)
+
+                print(f"Generated password: {generated_password}")
+                # TODO: Implement generator()
+
+            elif choice == '4':
+                print("Goodbye!")
+                break
+
+            else:
+                print("Invalid choice. Please try again.")
+            time.sleep(4)
+        else:
+            print("Incorrect password. Please try again.")
