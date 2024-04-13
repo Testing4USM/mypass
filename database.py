c2785b098fb6167c212680032a1718604eff633c
Neerixx <nerixmapper@hotmail.com>
2024-04-12 22:39:52 -0400

    refactor: remove unnecessary imports and modify logic

--- database.py
+++ database.py
@@ -1,7 +1,4 @@
 import sqlite3 as sql
-from hashlib import md5
-from utils import encrypt
-from utils import decrypt
 
 class Database:
     USERS_TABLE = 'users'
@@ -27,36 +24,35 @@ class Database:
         self.cursor.execute(STATEMENT)
         self.conn.commit()
 
-    def create_user(self, user, email, password) -> None:
-        hashed_password = md5(password.encode()).hexdigest()
+    def create_user(self, user, email, password) -> bool:
         statement = f"INSERT INTO {self.USERS_TABLE} (username, email, password) VALUES (?, ?, ?)"
 
         try:
-            self.cursor.execute(statement, (user, email, hashed_password))
+            self.cursor.execute(statement, (user, email, password))
             self.conn.commit()
+
+            return True
         except sql.IntegrityError:
-            print("User already exists")
+            return False
 
     def login_user(self, email, password) -> tuple:
-        hashed_password = md5(password.encode()).hexdigest()
         statement = f"SELECT * FROM {self.USERS_TABLE} WHERE email = ? AND password = ?"
 
-        self.cursor.execute(statement, (email, hashed_password))
+        self.cursor.execute(statement, (email, password))
         user = self.cursor.fetchone()
 
         return user
     
-    def add_password(self, id, name, password: str) -> None:
-        token = encrypt(password)
+    def add_password(self, id, name, password: str) -> tuple:
         statement = f"INSERT INTO {self.KEYCHAIN_TABLE} (name, password, user) VALUES (?, ?, ?)"
 
         try:
-            self.cursor.execute(statement, (name, token, id))
+            self.cursor.execute(statement, (name, password, id))
             self.conn.commit()
         except sql.IntegrityError:
-            print("Password already exists")
+            return (False, None)
 
-        return token
+        return (True, password)
     
     def get_passwords(self, id) -> list:
         statement = f"SELECT id, name, password FROM {self.KEYCHAIN_TABLE} WHERE user = ?"
@@ -67,10 +63,9 @@ class Database:
         return passwords
     
     def change_password(self, name, password) -> None:
-        token = encrypt(password)
         statement = f"UPDATE {self.KEYCHAIN_TABLE} SET password = ? WHERE name = ?"
 
-        self.cursor.execute(statement, (token, name))
+        self.cursor.execute(statement, (password, name))
         self.conn.commit()
 
     def delete_password(self, name) -> None:
